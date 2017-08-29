1f6b6ea675faad6b4cf9a806825df860c91a019a
Julio Cruz <atlascruz009@gmail.com>
2017-08-28 21:03:08 -0700

    Delete Article

--- app.js
+++ app.js
@@ -74,6 +74,69 @@ app.post('/articles/add', function(req,res){
     });
 });
 
+// Get a single article
+app.get('/article/:id', function(req,res){
+    Article.findById(req.params.id, function(err, article){
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.render('article', {
+                article: article 
+             });
+        }
+    });
+});
+
+// Get a edit article
+app.get('/article/edit/:id', function(req,res){
+    Article.findById(req.params.id, function(err, article){
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.render('edit_article', {
+                title: "Edit Article",
+                article: article
+            });
+        }
+    });
+});
+
+// Post Edit Article
+app.post('/articles/edit/:id', function(req,res){
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id};
+
+    Article.update(query, article, function(err){
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Edit: ', article);
+            res.redirect('/');
+        }
+    });
+
+});
+
+// Delete Article
+app.delete('/articles/:id', function(req,res){
+    let query = {_id: req.params.id};
+    Article.remove(query, function(err){
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.send('Success');
+        }
+    });
+});
+
 // Listening Port
 app.listen(3000, function(){
     console.log("Server started on port 3000...");
